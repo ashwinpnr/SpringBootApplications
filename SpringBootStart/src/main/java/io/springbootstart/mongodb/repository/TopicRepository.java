1caaad288538c048c103146e42a81b1d41510692
Ashwin P P <ashwin.pnr007@gmail.com>
2020-05-04 11:26:40 +0530

    Deleting unwanted files

--- SpringBootStart/src/main/java/io/springbootstart/mongodb/repository/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package io.springbootstart.mongodb.repository;
-
-import io.springbootstart.models.Topic;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TopicRepository extends MongoRepository<Topic,String> {
-
-}
-
